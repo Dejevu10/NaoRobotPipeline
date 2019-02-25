9674e3c5fdae91ec86d6d6c28a42eca5c8864b6e
Dejevu10 <30285417+Dejevu10@users.noreply.github.com>
2019-02-25 13:16:09 +0100

    Push

---
+++ src/main/java/de/naorobot/pipeline/NaoRobotPipelineApplication.java
@@ -0,0 +1,13 @@
+package de.naorobot.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaoRobotPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaoRobotPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/naorobot/pipeline/NaoRobotPipelineApplicationTests.java
@@ -0,0 +1,16 @@
+package de.naorobot.pipeline;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NaoRobotPipelineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
